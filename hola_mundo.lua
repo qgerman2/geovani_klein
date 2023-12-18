b2f61aea0635eae7849424d4fd7e747bece323b5
qgerman2 <qgerman2@gmail.com>
2023-12-18 18:08:30 -0300

    ahora si

--- hola_mundo.lua
+++ hola_mundo.lua
@@ -1,6 +1,6 @@
 local baud_rate = 115200
 
-local port = assert(serial:find_serial(0), "couldnt find serial port")
+local port = assert(serial:find_serial(0), "LUA: Puerto serial no configurado")
 
 port:begin(baud_rate)
 port:set_flow_control(0)
@@ -13,7 +13,16 @@ local header_count = 0
 local bytes_to_read = 0
 local buffer = ""
 
+local new_save_time = 0
+
 local max_bytes_per_update = 100
+local lecturas = 0
+
+function inicio()
+    gcs:send_text(0, "LUA: Iniciado")
+    new_save_time = millis() + 2000
+    return update, 100
+end
 
 function update()
     local bytes = math.min(port:available(), max_bytes_per_update)
@@ -32,30 +41,55 @@ function update()
         elseif bytes_to_read == -1 then
             bytes_to_read = n
             buffer = {}
-            gcs:send_text(0, "hay que leer " .. bytes_to_read .. " bytes")
         elseif bytes_to_read > 0 then
             table.insert(buffer, string.char(n))
             bytes_to_read = bytes_to_read - 1
             if bytes_to_read == 0 then
-                gcs:send_text(0, "lectura completa de " .. #buffer)
+                lecturas = lecturas + 1
                 data = table.concat(buffer)
-                values = table.pack(string.unpack("<fffff", data))
+                values = table.pack(string.unpack("<ffffff", data))
                 table.remove(values, #values)
-                for i, v in ipairs(values) do
-                    file:write(v)
-                    if i < #values then
-                        file:write("")
-                    end
-                    gcs:send_text(0, i .. ": " .. v)
+                str = tostring(millis()) .. ","
+                str = str .. table.concat(values, ",") .. ","
+                local accel = ahrs:get_accel()
+                local gyro = ahrs:get_gyro()
+                local pos = ahrs:get_position()
+                local vel = ahrs:get_velocity_NED()
+                local wind = ahrs:wind_estimate()
+                local airspeed = ahrs:airspeed_estimate()
+                str = str .. table.concat({accel:x(), accel:y(), accel:z()}, ",") .. ","
+                str = str .. table.concat({gyro:x(), gyro:y(), gyro:z()}, ",") .. ","
+                str = str .. table.concat({ahrs:get_roll(), ahrs:get_pitch(), ahrs:get_yaw()}, ",") .. ","
+                if (pos == nil) then
+                    str = str .. "0,0,0,"
+                else
+                    str = str .. table.concat({pos:lat(), pos:lng(), pos:alt()}, ",") .. ","
                 end
-                str = table.concat(values, ",") .. '\n'
+                if (vel == nil) then
+                    str = str .. "0,0,0,"
+                else
+                    str = str .. table.concat({vel:x(), vel:y(), vel:z()}, ",") .. ","
+                end
+                str = str .. table.concat({wind:x(), wind:y(), wind:z()}, ",") .. ","
+                if (airspeed == nil) then
+                    str = str .. "0\n"
+                else
+                    str = str .. tostring(airspeed) .. "\n"
+                end
                 file:write(str)
-                file:flush()
             end
         end
         bytes = bytes - 1
     end
-    return update, 100
+    local now = millis()
+    if (now > new_save_time) then
+        new_save_time = now + 2000
+        gcs:send_text(0, "LUA: Guardadas " .. lecturas .. " lecturas en la SD")
+        lecturas = 0
+        file:flush()
+    end
+    
+    return update, 1
 end
 
-return update()
+return inicio, 100
--- sketch_nov10a.ino
+++ sketch_nov10a.ino
@@ -1,29 +1,117 @@
+#include <SoftwareWire.h>
+#define sensor_direc 0x28
+
+SoftwareWire myWire(2, 3);
+SoftwareWire myWire2(4, 5);
+
 #include <SoftwareSerial.h>
+const byte rxPin = 7;
+const byte txPin = 6;
+SoftwareSerial SerialAPixHawk(rxPin, txPin);
 
-const byte rxPin = 2;
-const byte txPin = 3;
+float offset;
+const int veleta_AOA = A1;
+const int alimentacion = A0;
+const int veleta_SSA = A2;
 
-// Set up a new SoftwareSerial object
-SoftwareSerial mySerial (rxPin, txPin);
+
+float deltapres_fwd, deltapres_45;
+float pfwdnocarga, p45nocarga, pfwdnocargapsi, p45nocargapsi;
+float pfwdcorr_pas, p45corr_pas;
+int lectura1 = 0;
+int lectura45 = 0;
+int valor = 63;
+int Pmax, Pmin, cuenta;
+float l45, lfwd;
+float pfwd_p45; //relacion entre pfwd y p45
+float relacion, lfwd_corr, l45_corr;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9800);
-  mySerial.begin(115200);
-  Serial.println("HOLAAAA");
+    Serial.begin(9600);
+    myWire.begin();
+    myWire2.begin();
+    SerialAPixHawk.begin(115200);
+
+    pfwdnocarga = 8190; //corroborar bn estos valores
+    p45nocarga = 8230; //corroborar bn estos valores
+    cuenta = 16383;
+    Pmax = 1; //psi
+    Pmin = -1; //psi
+    pfwdnocargapsi = ((pfwdnocarga - (0.1 * cuenta)) * ((Pmax - Pmin) / (0.8 * cuenta)) + Pmin);
+    p45nocargapsi = ((p45nocarga - (0.1 * cuenta)) * ((Pmax - Pmin) / (0.8 * cuenta)) + Pmin);
+
 }
 
 void loop() {
-  struct {
-    float a = 1.0;
-    float b = 2.0;
-    float c = 3.0;
-    float d = 4.0;
-    float e = 5.0;
-  } valores;
-  uint8_t size = sizeof(valores);
-  mySerial.print("init");
-  mySerial.write(size);
-  mySerial.write(reinterpret_cast<uint8_t*>(&valores), size);
-  delay(10);
+    //conexion con el sensor fwd
+    myWire.beginTransmission(sensor_direc);
+    myWire.write(0);
+    myWire.endTransmission();
+    myWire.requestFrom(sensor_direc, 2);
+    if (2 <= myWire.available()) {
+        lectura1 = myWire.read();
+        lectura1 = lectura1 & valor;
+        lectura1 = lectura1 << 8;
+        lectura1 |= myWire.read();
+    }
+    //conexion con el sensor p45
+    myWire2.beginTransmission(sensor_direc);
+    myWire2.write(0);
+    myWire2.endTransmission();
+    myWire2.requestFrom(sensor_direc, 2);
+    if (2 <= myWire2.available()) {
+        lectura45 = myWire2.read();
+        lectura45 = lectura45 & valor;
+        lectura45 = lectura45 << 8;
+        lectura45 |= myWire2.read();
+
+    }
+    //lecturas de la presion en bytes como float 
+    l45 = lectura45;
+    lfwd = lectura1;
+    lfwd_corr = lfwd - pfwdnocarga;
+    l45_corr = l45 - p45nocarga;
+    deltapres_fwd = ((lfwd - (0.1 * cuenta)) * ((Pmax - Pmin) / (0.8 * cuenta)) + Pmin) - pfwdnocargapsi;   //presion en psi TIPO A  corregida
+    deltapres_45 = ((l45 - (0.1 * cuenta)) * ((Pmax - Pmin) / (0.8 * cuenta)) + Pmin) - p45nocargapsi;    //presion en psi TIPO A  corregida
+    pfwdcorr_pas = deltapres_fwd * 6894.75; //persion en fwd en pascales
+    p45corr_pas = deltapres_45 * 6894.75;  //presion en 45 en pascales
+
+    pfwd_p45 = pfwdcorr_pas / p45corr_pas;  //relacion entre presion fwd y 45
+    relacion = lfwd_corr / l45_corr;
+
+
+    //LECTURAS DE LAS VELETAS
+    int lectura_veleta_AOA = analogRead(veleta_AOA);
+    int lectura_alimentacion = analogRead(alimentacion);
+    int lectura_veleta_SSA = analogRead(veleta_SSA);
+    offset = 0.06;
+    float voltage_AOA = offset + lectura_veleta_AOA * (5.0 / 1023.0);
+    float alimentacion = offset + lectura_alimentacion * (5.0 / 1023.0);
+    float voltage_SSA = offset + lectura_veleta_SSA * (5.0 / 1023.0);
+
+
+    struct valores {
+        float a;
+        float b;
+        float c;
+        float d;
+        float e;
+        float f;
+    };
+    valores v = {
+      pfwdcorr_pas,
+      p45corr_pas,
+      pfwd_p45,
+      alimentacion,
+      voltage_AOA,
+      voltage_SSA
+    };
+    uint8_t size = sizeof(v);
+    SerialAPixHawk.print("init");
+    SerialAPixHawk.write(size);
+    SerialAPixHawk.write(reinterpret_cast<uint8_t *>(&v), size);
+
+    delay(10);
+
 }
